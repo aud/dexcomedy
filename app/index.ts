dad1f6af9fcbe50889caa5f0d2dcbc8e8b680079
Elliot Dohm <elliot.dohm@shopify.com>
2021-02-20 18:41:41 -0500

    initial structure

--- app/index.ts
+++ app/index.ts
@@ -1,12 +1,39 @@
 import document from "document";
 import asap from "fitbit-asap/app"
+import {Payload} from '../common/types;
 // import {me} from "appbit";
 
 const data = document.getElementById("Mmol")
 
-asap.onmessage = message => {
-  console.error(JSON.stringify(message))
+// "alerting":{"enabled":true,"lowThreshold":"4.4","highThreshold":"12.3"},
+// "weather":{"enabled":true,"unit":"fahrenheit","temperature":275.48}},
+// "gloucose":{"lastUpdatedSec":301,"trend":"steady","value":14.3,"unit":"mmol"},
+// "clock":{"format":"12"},
+
+const drawGloucose(gloucose) {
 }
+
+asap.onmessage = ({alerting, weather, gloucose, clock}: Payload) => {
+  console.log("Got weather: ", JSON.stringify(weather));
+  console.log("Got alerting: ", JSON.stringify(alerting));
+  console.log("Got gloucose: ", JSON.stringify(gloucose));
+  console.log("Got clock: ", JSON.stringify(clock));
+
+  // if (weather.enabled) {
+  //   drawWeather();
+  // }
+
+  // if (alerting.enabled) {
+  //   drawAlerting();
+  // }
+
+  // drawGloucose();
+  // drawClock();
+  // drawHeartRate();
+  // drawDate();
+  // drawSteps();
+}
+
 
 // console.error(data)
 // setTimeout(() => {
